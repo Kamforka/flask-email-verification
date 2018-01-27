3ee57a603e1353ba04792046f9866246cf0e5e96
Szabolcs Antal <kamforka@hotmail.com>
2018-01-27 16:53:14 +0100

    Add utility methods to generate/check confirmation token and send mails.

--- vimcar/utils.py
+++ vimcar/utils.py
@@ -1,2 +1,29 @@
 # -*- coding: utf-8 -*-
 """Helper utilities and decorators."""
+
+from itsdangerous import URLSafeTimedSerializer
+from vimcar.settings import Config
+from flask_mail import Message
+from vimcar.extensions import mail
+
+
+def generate_confirmation_token(email):
+    """Confirmation email token."""
+    serializer = URLSafeTimedSerializer(Config.SECRET_KEY)
+    return serializer.dumps(email, salt=Config.SECURITY_SALT)
+
+
+def confirm_token(token, expiration=3600):
+    """Plausibility check of confirmation token."""
+    serializer = URLSafeTimedSerializer(Config.SECRET_KEY)
+    try:
+        email = serializer.loads(token, salt=Config.SECURITY_SALT, max_age=expiration)
+    except:
+        return False
+    return email
+
+
+def send_email(to, subject, template):
+    """Send an email."""
+    msg = Message(subject, recipients=[to], html=template)
+    mail.send(msg)
