f37c96bb2e342a57e0eafbe645972adb8cf0ed78
Szabolcs Antal <kamforka@hotmail.com>
2018-01-27 16:52:05 +0100

    Add needed extensions, remove unnecessary ones.

--- vimcar/extensions.py
+++ vimcar/extensions.py
@@ -1,15 +1,13 @@
 # -*- coding: utf-8 -*-
 """Extensions module. Each extension is initialized in the app factory located in app.py."""
 from flask_bcrypt import Bcrypt
-from flask_caching import Cache
-from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
 
 api = Api()
 bcrypt = Bcrypt()
-cors = CORS()
 db = SQLAlchemy()
+mail = Mail()
 migrate = Migrate()
-cache = Cache()
